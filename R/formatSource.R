d43e971287c09ba6e06c8f364441605818555015
Nathan Stephens <nathan@rstudio.com>
2016-04-08 16:18:27 -0400

    adding formatSource

---
+++ R/formatSource.R
@@ -0,0 +1,8 @@
+formatSource <- function() {
+  
+  context <- rstudioapi::getActiveDocumentContext()
+  formatted <- formatR::tidy_source(text = context$contents, output = FALSE)
+  contents <- paste0(formatted$text.tidy, collapse = "\n")
+  rstudioapi::setDocumentContents(contents, id = context$id)
+  
+}
